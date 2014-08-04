9a07d13a4f0555741956f11893b37809db3d514f
Nick <nick.dhewitt@gmail.com>
2014-08-04 12:47:06 -0700

    wrote tests for multivariate learning, stubbed out method.

---
+++ kryptnostic-core/src/main/java/com/kryptnostic/multivariate/learning/MultivariateLearning.java
@@ -0,0 +1,19 @@
+package com.kryptnostic.multivariate.learning;
+
+import com.kryptnostic.multivariate.gf2.PolynomialFunction;
+
+
+
+public class MultivariateLearning {
+	
+	/**
+	 * Given a polynomial and an assumed order of that polynomial, computes the inverse.
+	 * @param function
+	 * @param order
+	 * @return
+	 */
+	public static PolynomialFunction learnInverse(PolynomialFunction function, Integer order) {
+		return null;
+	}
+	
+}
---
+++ kryptnostic-core/src/test/java/com/kryptnostic/multivariate/learning/test/MultivariateLearningTests.java
@@ -0,0 +1,45 @@
+package com.kryptnostic.multivariate.learning.test;
+
+import java.util.Random;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cern.colt.bitvector.BitVector;
+
+import com.kryptnostic.linear.BitUtils;
+import com.kryptnostic.multivariate.PolynomialFunctions;
+import com.kryptnostic.multivariate.gf2.PolynomialFunction;
+import com.kryptnostic.multivariate.learning.MultivariateLearning;
+
+
+public class MultivariateLearningTests {
+	private static final Logger logger = LoggerFactory.getLogger( MultivariateLearningTests.class );
+	private static final Random r = new Random(0);
+	private static final Integer testPolynomialOrder = 2;
+	private static final Integer testPolynomialInputLength = 128;
+	private static final Integer testPolynomialOutputLength = 256;
+	
+	/**
+	 * Tests that learning is accurate when the learning function is given an accurate test polynomial order. 
+	 */
+	@Test
+	public void learnInverseTest() {
+		logger.info("Generating function to invert.");
+		PolynomialFunction function =  PolynomialFunctions.randomFunction(testPolynomialInputLength, testPolynomialOutputLength, 4, testPolynomialOrder); // TODO randomly generate number of terms by order
+		logger.info("Learning inverse function.");
+		PolynomialFunction inverse = MultivariateLearning.learnInverse(function, testPolynomialOrder);
+		
+		BitVector input = BitUtils.randomVector(testPolynomialInputLength);
+		BitVector output = function.apply(input);
+		
+		BitVector invertedOutput = inverse.apply(output);
+		
+		Assert.assertTrue(invertedOutput.equals(input));
+	}
+	
+	
+}
