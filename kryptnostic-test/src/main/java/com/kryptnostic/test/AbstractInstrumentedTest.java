e71acc44cc2f5e19db22ce8f305ef9f5f7133b1d
Matthew Tamayo <matthew@kryptnostic.com>
2014-08-15 22:01:37 -0700

    Partial evaluation implemented for search.
    It is now possible to provide partial input to SimplePolynomialFunction by calling resolve. This results in another SimplePolynomialFunction, which is capable of evaluating the remaining bits.
    
    Also updated is the test driver, with better support for running automated tests that push test metrics to a graphite instance for measuring perf.

--- kryptnostic-test/src/main/java/com/kryptnostic/test/AbstractInstrumentedTest.java
+++ kryptnostic-test/src/main/java/com/kryptnostic/test/AbstractInstrumentedTest.java
@@ -1,32 +1,27 @@
 package com.kryptnostic.test;
 import java.util.concurrent.ConcurrentMap;
 
-import org.junit.Before;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Maps;
 import com.kryptnostic.test.metrics.MetricsConfiguration;
 
 
-public abstract class AbstractInstrumentedTest {
+@Configuration
+public class AbstractInstrumentedTest {
 	private static final ConcurrentMap<Class<?>,AnnotationConfigApplicationContext> contexts = Maps.newConcurrentMap();
 	
-	@Before
-    public void refreshContext() {
-	    AnnotationConfigApplicationContext context = getTestContext();
-        if( !context.isActive() ) {
-            context.register( MetricsConfiguration.class );
-            context.refresh();
-        }
-    }
-	
-	protected static void registerTestConfigurationsForClass( Class<?> clazz , Class<?> ... annotatedClasses ) {
-	    AnnotationConfigApplicationContext context = getTestContext( clazz );
-	    context.register( annotatedClasses );
+	public AbstractInstrumentedTest( Class<?> ... annotatedClasses ) {
+	    registerTestConfigurations( MetricsConfiguration.class );
+	    if( annotatedClasses!=null && annotatedClasses.length > 0 ) {
+	        registerTestConfigurations( annotatedClasses );
+	    }
+	    getTestContext().refresh();
 	}
 	
-	protected static AnnotationConfigApplicationContext getTestContext( Class<?> clazz ) {
+	private static AnnotationConfigApplicationContext getTestContext( Class<?> clazz ) {
 	    Preconditions.checkNotNull( clazz , "Cannot retrieve context for null class." );
 	    
 	    AnnotationConfigApplicationContext context = contexts.get( clazz);
@@ -45,6 +40,7 @@ public abstract class AbstractInstrumentedTest {
 	protected void registerTestConfigurations( Class<?> ... annotatedClasses ) {
 	    AnnotationConfigApplicationContext context = getTestContext();
 	    Preconditions.checkState( !context.isActive() , "Context cannot be active." );
+	    context.register( annotatedClasses );
 	}
 
 }
