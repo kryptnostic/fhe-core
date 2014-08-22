dfaeaeb6b20aa77c8bba692df059911fff69f171
Matthew Tamayo <matthew@kryptnostic.com>
2014-08-21 23:45:01 -0700

    Removed unnecessary files

--- kryptnostic-api/src/main/java/com/kryptnostic/search/DocumentSearchService.java
+++
@@ -1,9 +0,0 @@
-package com.kryptnostic.search;
-
-import java.util.Set;
-
-import com.kryptnostic.indexing.metadata.Metadatum;
-
-public interface DocumentSearchService {
-	public Set<Metadatum> search( String query );
-}
