eca37775b8ced9951dec5ba7e753413c4119f2f3
Sina Iman <sina@kryptnostic.com>
2014-09-10 16:34:29 -0700

    annotated ciphertext class

--- kryptnostic-core/src/main/java/com/kryptnostic/crypto/Ciphertext.java
+++ kryptnostic-core/src/main/java/com/kryptnostic/crypto/Ciphertext.java
@@ -1,22 +1,32 @@
 package com.kryptnostic.crypto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
- * Ciphertext envelope for encrypted data. 
+ * Ciphertext envelope for encrypted data.
+ * 
  * @author Matthew Tamayo-Rios
  */
 public class Ciphertext {
-    private final byte[] contents; //Encrypted field
-    private final long[] length;   //Encrypted field
-    
-    public Ciphertext( byte[] contents , long [] length ) {
+    public static final String FIELD_CONTENTS = "contents";
+    public static final String FIELD_LENGTH = "length";
+
+    private final byte[] contents; // Encrypted field
+    private final long[] length; // Encrypted field
+
+    @JsonCreator
+    public Ciphertext(@JsonProperty(FIELD_CONTENTS) byte[] contents, @JsonProperty(FIELD_LENGTH) long[] length) {
         this.length = length;
         this.contents = contents;
     }
 
+    @JsonProperty(FIELD_CONTENTS)
     public byte[] getContents() {
         return contents;
     }
-    
+
+    @JsonProperty(FIELD_LENGTH)
     public long[] getLength() {
         return length;
     }
