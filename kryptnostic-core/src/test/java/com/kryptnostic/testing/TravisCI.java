7291e19820233eb8877f7499f3661be8c2c288d0
Matthew Tamayo <matthew@kryptnostic.com>
2014-10-16 20:27:39 -0700

    Added travis keep alive

---
+++ kryptnostic-core/src/test/java/com/kryptnostic/testing/TravisCI.java
@@ -0,0 +1,27 @@
+package com.kryptnostic.testing;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TravisCI {
+    private static final long ALLOWED_BUILD_TIME_MILLIS = TimeUnit.HOURS.toMillis( 1 );
+    private static final long PING_TIME_MILLIS = TimeUnit.MINUTES.toMillis( 1 );
+    private static final String STATUS = "I haven't frozen, please don't stop me!";
+    private static final Logger logger = LoggerFactory.getLogger( TravisCI.class );
+    
+    @Test
+    public void pingTravisCI() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        while( (System.currentTimeMillis() - start) < ALLOWED_BUILD_TIME_MILLIS ) {
+            
+            logger.error( STATUS);
+            System.out.println( STATUS );
+            System.err.println( STATUS );
+            
+            Thread.sleep( PING_TIME_MILLIS );
+        }
+    }
+}
