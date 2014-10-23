24a3b803575ddce6d85c75e662ed2b1afb01a54c
Sina Iman <sina@kryptnostic.com>
2014-10-23 00:06:02 -0700

    annotate EncryptedSearchBridgeKey

--- kryptnostic-core/src/main/java/com/kryptnostic/crypto/EncryptedSearchBridgeKey.java
+++ kryptnostic-core/src/main/java/com/kryptnostic/crypto/EncryptedSearchBridgeKey.java
@@ -1,15 +1,27 @@
 package com.kryptnostic.crypto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.kryptnostic.linear.EnhancedBitMatrix;
 import com.kryptnostic.linear.EnhancedBitMatrix.SingularMatrixException;
 
-
 public class EncryptedSearchBridgeKey {
+    public static final String FIELD_BRIDGE = "bridge";
+
     private final EnhancedBitMatrix bridge;
-    public EncryptedSearchBridgeKey( EncryptedSearchPrivateKey privateKey , EncryptedSearchSharingKey sharingKey ) throws SingularMatrixException {
-        this.bridge = privateKey.getSquaringMatrix().inverse().multiply( sharingKey.getMiddle() ).multiply( privateKey.getSquaringMatrix() );
+
+    public EncryptedSearchBridgeKey(EncryptedSearchPrivateKey privateKey, EncryptedSearchSharingKey sharingKey)
+            throws SingularMatrixException {
+        this.bridge = privateKey.getSquaringMatrix().inverse().multiply(sharingKey.getMiddle())
+                .multiply(privateKey.getSquaringMatrix());
     }
 
+    @JsonCreator
+    public EncryptedSearchBridgeKey(@JsonProperty(FIELD_BRIDGE) EnhancedBitMatrix bridge) {
+        this.bridge = bridge;
+    }
+
+    @JsonProperty(FIELD_BRIDGE)
     public EnhancedBitMatrix getBridge() {
         return bridge;
     }
