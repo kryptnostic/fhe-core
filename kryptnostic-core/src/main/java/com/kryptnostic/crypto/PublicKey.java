974086b60afeea3b4916aad7d99c529fef13bbdd
Nick Hewitt <nick@kryptnostic.com>
2014-10-25 00:56:03 -0700

    Make FHE PublicKey, PaddingStrategy and ZeroPaddingStrategy jackson serializable

--- kryptnostic-core/src/main/java/com/kryptnostic/crypto/PublicKey.java
+++ kryptnostic-core/src/main/java/com/kryptnostic/crypto/PublicKey.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 
 import cern.colt.bitvector.BitVector;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Preconditions;
 import com.kryptnostic.crypto.padding.PaddingStrategy;
 import com.kryptnostic.crypto.padding.ZeroPaddingStrategy;
@@ -15,105 +17,138 @@ import com.kryptnostic.multivariate.gf2.SimplePolynomialFunction;
 import com.kryptnostic.multivariate.polynomial.OptimizedPolynomialFunctionGF2;
 
 /**
- * Public key class used for encryption. 
+ * Public key class used for encryption.
+ * 
  * @author Matthew Tamayo-Rios
  */
 public class PublicKey {
-    private static final Logger logger = LoggerFactory.getLogger( PublicKey.class );
-    //TODO: Replace with bouncy castle or real number generator.
-    private static final Random r = new Random( 0 );
-    private final SimplePolynomialFunction encrypter;
-    private final SimplePolynomialFunction m;
+    private static final String              ENCRYPTER_PROPERTY        = "encrypter";
+    private static final String              M_PROPERTY                = "m";
+    private static final String              COMPLEXITY_CHAIN_PROPERTY = "complexity-chain";
+    private static final String              PADDING_STRATEGY_PROPERTY = "padding-strategy";
+    private static final String              LONGS_PER_BLOCK_PROPERTY  = "longs-per-block";
+
+    private static final Logger              logger                    = LoggerFactory.getLogger( PublicKey.class );
+    // TODO: Replace with bouncy castle or real number generator.
+    private static final Random              r                         = new Random( 0 );
+    private final SimplePolynomialFunction   encrypter;
+    private final SimplePolynomialFunction   m;
     private final SimplePolynomialFunction[] complexityChain;
-    private final PaddingStrategy paddingStrategy;
-    private final int longsPerBlock;
-    
+    private final PaddingStrategy            paddingStrategy;
+    private final int                        longsPerBlock;
+
+    @JsonCreator
+    public PublicKey(
+            @JsonProperty( ENCRYPTER_PROPERTY ) SimplePolynomialFunction encrypter,
+            @JsonProperty( M_PROPERTY ) SimplePolynomialFunction m,
+            @JsonProperty( COMPLEXITY_CHAIN_PROPERTY ) SimplePolynomialFunction[] complexityChain,
+            @JsonProperty( PADDING_STRATEGY_PROPERTY ) PaddingStrategy paddingStrategy,
+            @JsonProperty( LONGS_PER_BLOCK_PROPERTY ) int longsPerBlock ) {
+        this.encrypter = encrypter;
+        this.m = m;
+        this.complexityChain = complexityChain;
+        this.paddingStrategy = paddingStrategy;
+        this.longsPerBlock = longsPerBlock;
+    }
+
     public PublicKey( PrivateKey privateKey ) {
         this( privateKey, new ZeroPaddingStrategy( privateKey.getE1().rows() >>> 4 ) );
     }
-    
-    public PublicKey( PrivateKey privateKey , PaddingStrategy paddingStrategy ) {
+
+    public PublicKey( PrivateKey privateKey, PaddingStrategy paddingStrategy ) {
         this.paddingStrategy = paddingStrategy;
-        int inputLen =  privateKey.getE1().cols();
+        int inputLen = privateKey.getE1().cols();
         int outputLen = privateKey.getE1().rows();
         complexityChain = null;
-        m = OptimizedPolynomialFunctionGF2.truncatedIdentity( inputLen , outputLen );
-        logger.debug( "m: {} -> {}" , outputLen , inputLen );
-        
+        m = OptimizedPolynomialFunctionGF2.truncatedIdentity( inputLen, outputLen );
+        logger.debug( "m: {} -> {}", outputLen, inputLen );
+
         /*
          * E(m) = E1(m + h + Ag ) + E2(m + h + Bg )
          */
-        
-        encrypter = privateKey.encrypt( m ); 
-        logger.debug("Required input length in bits: {}" , encrypter.getInputLength() );
+
+        encrypter = privateKey.encrypt( m );
+        logger.debug( "Required input length in bits: {}", encrypter.getInputLength() );
         // 8 bits per byte, 8 bytes per long.
         longsPerBlock = encrypter.getInputLength() >>> 7;
     }
-    
+
     public Ciphertext encryptIntoEnvelope( byte[] plaintext ) {
-        long[] lengthArray = new long[ longsPerBlock<<1 ];
-        
-        lengthArray[0] = plaintext.length;
-        for( int i = 1 ; i < lengthArray.length ; ++i ) {
+        long[] lengthArray = new long[ longsPerBlock << 1 ];
+
+        lengthArray[ 0 ] = plaintext.length;
+        for ( int i = 1; i < lengthArray.length; ++i ) {
             lengthArray[ i ] = r.nextLong();
         }
-        
-        return new Ciphertext( encrypt( plaintext ) , encrypt( lengthArray ) );
+
+        return new Ciphertext( encrypt( plaintext ), encrypt( lengthArray ) );
     }
-    
+
     byte[] encrypt( byte[] plaintext ) {
-        Preconditions.checkNotNull( plaintext , "Plaintext to be encrypted cannot be null." );
-        
-        /* 
-         * 1) Pad the data so it aligns 
+        Preconditions.checkNotNull( plaintext, "Plaintext to be encrypted cannot be null." );
+
+        /*
+         * 1) Pad the data so it aligns
          */
         plaintext = paddingStrategy.pad( plaintext );
-               
+
         ByteBuffer buffer = ByteBuffer.wrap( plaintext );
         ByteBuffer outBuf = ByteBuffer.allocate( plaintext.length << 1 );
-        
+
         int blockLen = longsPerBlock << 1;
-        while( buffer.remaining() > 0 ) {
+        while ( buffer.remaining() > 0 ) {
             long[] lpt = new long[ blockLen ];
-            
-            for( int i = 0 ; i < longsPerBlock; ++i ) {
-               lpt[i] = buffer.getLong();
+
+            for ( int i = 0; i < longsPerBlock; ++i ) {
+                lpt[ i ] = buffer.getLong();
             }
-            
-            for( int i = longsPerBlock; i < blockLen ; ++i ) {
-               lpt[i] = 0L;//r.nextLong();
+
+            for ( int i = longsPerBlock; i < blockLen; ++i ) {
+                lpt[ i ] = 0L;// r.nextLong();
             }
-            
+
             long[] ciphertext = encrypt( lpt );
-            for( long lct : ciphertext ) {
+            for ( long lct : ciphertext ) {
                 outBuf.putLong( lct );
             }
         }
-        
+
         return outBuf.array();
     }
-    
+
     long[] encrypt( long[] plaintext ) {
-        logger.debug( "Expected plaintext block length: {}" , encrypter.getInputLength() );
-        logger.debug( "Observed plaintext block length: {}" , plaintext.length * 8 * 8 );
-        Preconditions.checkArgument( (plaintext.length<<3) == ( encrypter.getInputLength() >>> 3 ) , "Cannot directly encrypt block of incorrect length." );
-        
-        BitVector result = encrypter.apply( new BitVector( plaintext , encrypter.getInputLength() ) );
-        if( logger.isDebugEnabled() ) {
-        	for( long l : result.elements() ) {
-        		logger.debug("Wrote the following ciphertext long: {}" , l );
-        	}
+        logger.debug( "Expected plaintext block length: {}", encrypter.getInputLength() );
+        logger.debug( "Observed plaintext block length: {}", plaintext.length * 8 * 8 );
+        Preconditions.checkArgument(
+                ( plaintext.length << 3 ) == ( encrypter.getInputLength() >>> 3 ),
+                "Cannot directly encrypt block of incorrect length." );
+
+        BitVector result = encrypter.apply( new BitVector( plaintext, encrypter.getInputLength() ) );
+        if ( logger.isDebugEnabled() ) {
+            for ( long l : result.elements() ) {
+                logger.debug( "Wrote the following ciphertext long: {}", l );
+            }
         }
         return result.elements();
     }
-    
-    //public abstract byte[] encryptObject( Object object );
-    
+
     public SimplePolynomialFunction getEncrypter() {
         return encrypter;
     }
 
     public SimplePolynomialFunction getM() {
         return m;
+    }
+
+    public SimplePolynomialFunction[] getComplexityChain() {
+        return complexityChain;
+    }
+
+    public PaddingStrategy getPaddingStrategy() {
+        return paddingStrategy;
+    }
+
+    public int getLongsPerBlock() {
+        return longsPerBlock;
     }
 }
--- kryptnostic-core/src/main/java/com/kryptnostic/crypto/padding/ZeroPaddingStrategy.java
+++ kryptnostic-core/src/main/java/com/kryptnostic/crypto/padding/ZeroPaddingStrategy.java
@@ -2,10 +2,15 @@ package com.kryptnostic.crypto.padding;
 
 import java.util.Arrays;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ZeroPaddingStrategy implements PaddingStrategy {
+    private static final String INPUT_BYTE_LENGTH_PROPERTY = "input-byte-length";
     public final int inputByteLength;
     
-    public ZeroPaddingStrategy( int inputByteLength ) {
+    @JsonCreator
+    public ZeroPaddingStrategy( @JsonProperty(INPUT_BYTE_LENGTH_PROPERTY) int inputByteLength ) {
         this.inputByteLength = inputByteLength;
     }
 
@@ -17,5 +22,10 @@ public class ZeroPaddingStrategy implements PaddingStrategy {
         } else {
             return unpadded;
         }
+    }
+   
+    @JsonProperty(INPUT_BYTE_LENGTH_PROPERTY)
+    public int getInputByteLength() {
+        return inputByteLength;
     }
 }
